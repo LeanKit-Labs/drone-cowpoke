9c1ca81d6ba7f2704f399a721a3c41fb68e0f8a3
Alex Robson <asrobson@gmail.com>
2016-02-23 20:41:40 -0600

    Add test dependency to drone file

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -o drone-cowpoke .
-docker rmi -f jaymedavis/drone-cowpoke
-docker build --no-cache --rm --force-rm=true -t jaymedavis/drone-cowpoke .
+docker rmi -f leankit/drone-cowpoke
+docker build --no-cache --rm --force-rm=true -t leankit/drone-cowpoke .
